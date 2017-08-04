409245447e3d87eb482eb2a5e2d03dd4271530ea
Nate Graham <avianate@ldschurch.org>
2017-08-04 13:21:05 -0600

    Add storybook and CSS modules

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -138,6 +138,7 @@ module.exports = {
         exclude: [
           /\.html$/,
           /\.(js|jsx)$/,
+          /\.story.js$/,
           /\.css$/,
           /\.json$/,
           /\.bmp$/,
@@ -187,6 +188,8 @@ module.exports = {
             loader: require.resolve('css-loader'),
             options: {
               importLoaders: 1,
+              modules: true,
+              localIdentName: "[name]__[local]___[hash:base64:5]"
             },
           },
           {
